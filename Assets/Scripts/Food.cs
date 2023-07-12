4b420ed5ab538f207a1eca55584a0fa8a3568ac6
rgsk <guptarahul70322@gmail.com>
2023-07-13 01:04:30 +0530

    found spawn at random position

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Food : MonoBehaviour {
+    public BoxCollider2D gridArea;
+    void Start() {
+        RandomizePosition();
+    }
+
+    void RandomizePosition() {
+        var bounds = gridArea.bounds;
+        var x = Random.Range(bounds.min.x, bounds.max.x);
+        var y = Random.Range(bounds.min.y, bounds.max.y);
+        transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+}
