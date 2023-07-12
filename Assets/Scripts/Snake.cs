f14e7b38cba19e561ce9cfd429ffb9e460565d0b
rgsk <guptarahul70322@gmail.com>
2023-07-13 01:23:28 +0530

    Snake body expansion

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Snake : MonoBehaviour {
     Vector2 _direction = Vector2.right;
+    List<Transform> _segments;
+    public Transform segmentPrefab;
     void Start() {
-
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
     }
 
     void Update() {
@@ -18,10 +22,25 @@ public class Snake : MonoBehaviour {
         }
     }
     void FixedUpdate() {
+        for (int i = _segments.Count - 1; i > 0; i--) {
+            _segments[i].position = _segments[i - 1].position;
+        }
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
             0.0f
         );
+    }
+
+    void Grow() {
+        var segment = Instantiate(segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Food") {
+            Grow();
+        }
     }
 }
